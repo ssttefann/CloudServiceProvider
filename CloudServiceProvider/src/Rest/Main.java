43c8f7dd72f4ffd19713ebbeb8959a69a8b00fbc
Stefan <stefan.kandic@yahoo.com>
2019-12-13 16:15:36 +0100

    napravio backend u sparku, nesto nece da ucita vuejs kako treba ali mrzi me sad da se cimam oko toga

---
+++ CloudServiceProvider/src/Rest/Main.java
@@ -0,0 +1,42 @@
+package Rest;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+import static spark.Spark.staticFiles;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+
+import spark.Request;
+import spark.Session;
+
+
+import Model.*;
+
+public class Main {
+
+    private static Gson g = new Gson();
+    private static ArrayList<User> users = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        port(8080);
+
+        //mapiramo na vuejs public folder
+        staticFiles.externalLocation(new File("CloudServiceProvider/WebContent/app/public").getCanonicalPath());
+
+
+        get("/test", (req, res) -> {
+            return "Works";
+        });
+
+        get("/rest/getUsers/", (req, res) -> {
+            res.type("application/json");
+            return g.toJson(users);
+        });
+
+    }
+
+}

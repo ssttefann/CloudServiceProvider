798067d9dbad76b8b4cbbbeb18f14f0a63ebbbd0
Stefan <stefan.kandic@yahoo.com>
2019-12-13 06:08:50 +0100

    napravio sam kostur neki u vuetify, ako ovo ne sme da se koristi upucacu se vrv

--- CloudServiceProvider/WebContent/app/src/router/index.js
+++ CloudServiceProvider/WebContent/app/src/router/index.js
@@ -1,14 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Admin from '../components/Admin'
+import Login from '../components/Login'
+import Register from '../components/Register'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/admin',
+    name: 'admin',
+    component: Admin
+  },
+  {
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
   },
   {
     path: '/about',
@@ -16,7 +34,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../components/About.vue')
   }
 ]
 

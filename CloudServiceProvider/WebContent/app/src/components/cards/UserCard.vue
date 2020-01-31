e08a5777e401cbd993678c47ef189a89e3cefe6b
Mijat019 <mijat.miletic98@gmail.com>
2020-01-31 06:12:12 +0100

    tomorrow comes today

--- CloudServiceProvider/WebContent/app/src/components/cards/UserCard.vue
+++ CloudServiceProvider/WebContent/app/src/components/cards/UserCard.vue
@@ -220,7 +220,6 @@ export default {
               "success",
               "bottom"
             ]);
-            this.$router.go();
             this.close();
           })
           .catch(err =>
